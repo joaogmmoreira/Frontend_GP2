9b2fdf7dd60cbb046fd8dcfe4f7ab2230729eb0d
weliton schitini <schitini1987@gmail.com>
2024-09-29 19:41:25 -0300

    Adição do footer

--- src/Scripts/DragonBall.js
+++ src/Scripts/DragonBall.js
@@ -1,25 +1,26 @@
-let url = `https://dragonball-api.com/api/characters?page=2&limit=30`
 
 
-fetch(url).then((resp)=>{
-    return resp.json()
-}).then((result)=>{
-  
-    result.items.forEach(element => {
-        
-        let div = document.createElement("div")
-        let img =  document.createElement("img")
-        img.src = element.image
-        let h3 = document.createElement("h3")
-        h3.innerText = element.name
-        let p = document.createElement("p")
-        p.innerText = `
-        ki: ${element.ki}
-        raça ${element.race}`
-        div.append(img,h3,p)
-        document.getElementById("personagens").appendChild(div)
-    });
-   
-}).catch((err)=>{
-    console.log(err)
-})
+function criarCardsPersonagens(personagem) {
+
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = personagem.image;
+    let h3 = document.createElement("h3")
+    h3.innerText = personagem.name
+    let p = document.createElement("p")
+    p.innerText = `
+    ki: ${personagem.ki}
+    raça ${personagem.race}`
+    div.append(img, h3, p)
+    document.getElementById("personagens").appendChild(div)
+
+
+}
+
+async function pegarPersonagens() {
+    let resposta = await fetch(`https://dragonball-api.com/api/characters?page=2&limit=30`)
+    let personagens = await resposta.json();
+    personagens.items.forEach(criarCardsPersonagens);
+}
+pegarPersonagens()
+
