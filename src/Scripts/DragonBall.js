bd734c80f0109456029dc06423e874da32dad42f
weliton schitini <schitini1987@gmail.com>
2024-09-30 18:41:22 -0300

    alterações no codigo

--- src/Scripts/DragonBall.js
+++ src/Scripts/DragonBall.js
@@ -1,8 +1,28 @@
 
+let exibirBtn = document.getElementById("exibirPersonagem")
+
+exibirBtn.addEventListener("click", () => {
+    if (exibirBtn.innerText == "Exibir personagens") {
+        exibirBtn.innerText = "Esconder personagens"
+        pegarPersonagens()
+    } else if (exibirBtn.innerText == "Esconder personagens") {
+        exibirBtn.innerText = "Exibir personagens"
+        let a = document.getElementsByClassName("cards")
+        console.log(a)
+        for (let i = 0; i < a.length; i *= 2) {
+            a[i].remove()
+        }
+
+    }
+})
+
+
+
 
 function criarCardsPersonagens(personagem) {
 
     let div = document.createElement("div");
+    div.className = "cards"
     let img = document.createElement("img");
     img.src = personagem.image;
     let h3 = document.createElement("h3")
@@ -12,6 +32,8 @@ function criarCardsPersonagens(personagem) {
     ki: ${personagem.ki}
     raça ${personagem.race}`
     div.append(img, h3, p)
+
+
     document.getElementById("personagens").appendChild(div)
 
 
@@ -21,6 +43,7 @@ async function pegarPersonagens() {
     let resposta = await fetch(`https://dragonball-api.com/api/characters?page=2&limit=30`)
     let personagens = await resposta.json();
     personagens.items.forEach(criarCardsPersonagens);
+
 }
-pegarPersonagens()
+
 
